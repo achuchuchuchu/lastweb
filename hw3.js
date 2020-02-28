bba6b2789a7bf364025f792f11f776a6b1648929
achu <>
2020-02-28 11:31:21 +0900

    最終段階

--- hw3.js
+++ hw3.js
@@ -3,3 +3,18 @@ $(window).on('load', function(){
     $(this).delay(5000).fadeOut("slow");
   });
 });
+$('.st')
+.on('mouseover', function() {
+  $(this).css('opacity', 0.5);
+})
+.on('mouseleave', function() {
+  $(this).css('opacity', 1);
+});
+
+$('.final').on('mouseover', function() {
+  $(".us1").fadeIn(4000);
+});
+
+$('.final').on('mouseleave', function() {
+  $(".us1").fadeOut(2000);
+});
